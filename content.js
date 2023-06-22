a2fbb80ebb272cbb76259e77da1eb9d0d7d43c3d
Sarik Karmacharya <sarik.karmacharya@gmail.com>
2023-06-22 09:56:35 +0545

    Add support for screenshot

--- content.js
+++ content.js
@@ -1,4 +1,4 @@
-chrome.runtime.onMessage.addListener( (msg, sender, sendResponse) => {
+chrome.runtime.onMessage.addListener((msg, sender, sendResponse) => {
     // If the received message has the expected format...
     if (msg.text === 'get_clockify_report') {
         let last_day_report = document.querySelector("entry-group");
@@ -13,42 +13,79 @@ chrome.runtime.onMessage.addListener( (msg, sender, sendResponse) => {
             entries.push({ title, client })
         })
 
-        list(entries);
-        grouped(entries);
+        html2canvas(last_day_report.querySelector(".cl-card")).then(
+            function (canvas) {
+                const dataURL = canvas.toDataURL('image/png')
+                const img = dataURL.replace(/^data:image\/(png|jpg);base64,/, "");
+                let link = "";
+
+                let formData = new FormData();
+                formData.append("image", img);
+                formData.append("type", "base64");
+
+                $.ajax({
+                    url: "https://api.imgur.com/3/image",
+                    type: "POST",
+                    datatype: "json",
+                    headers: {
+                      "Authorization": "Client-ID aca6d2502f5bfd8"
+                    },
+                    data: formData,
+                    success: function(response) {
+                      if (response.success) {
+                        link = response.data.link;
+                      }
+                    },
+                    cache: false,
+                    contentType: false,
+                    processData: false,
+                    async: false,
+                  });
+                
+                  list(entries, link);
+                  grouped(entries, link);
+
+            })       
 
         sendResponse(msg);
     }
 
-    function list(entries) {
+    function list(entries, link) {
         let list = "List view: \n\n"
         entries.forEach((entry) => {
             list += `• ${entry.title} (${entry.client}) \n`
         })
+        list += `\n\n`;
+        list += link;
+        list += `\n\n`;
         console.log(list);
     }
-    
-    function grouped(entries) {
+
+    function grouped(entries, link) {
         let output = "Grouped view: \n\n"
         entries.forEach((entry) => {
             list += `${entry.title} (${entry.client}) \n`
         })
-    
+
         let groups = entries.reduce(function (r, a) {
             r[a.client] = r[a.client] || [];
             r[a.client].push(a);
             return r;
         }, Object.create(null));
-    
+
         Object.keys(groups).forEach((client) => {
             output += client + ":\n"
-    
+
             groups[client].forEach(entry => {
                 output += `• ${entry.title} \n`
             })
-    
+
             output += "\n"
         })
-    
+
+        output += link;
+        output += `\n\n`;
+
         console.log(output)
     }
 });
