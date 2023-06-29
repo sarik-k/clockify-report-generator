ec4284b14be334d1bd4b070c5d59ce223115833d
Sarik Karmacharya <sarik.karmacharya@gmail.com>
2023-06-29 18:36:28 +0545

    WIP

--- js/popup.js
+++ js/popup.js
@@ -14,12 +14,14 @@ window.addEventListener('DOMContentLoaded', async () => {
                 { from: 'popup', subject: 'get_clockify_report' },
                 writeToPopup);
         } else {
-            $(".loading").hide();
-            $(".content-wrap").show();
-            $(".content-wrap").html("Not clockify tracker site")
+            $("#app").html("Not clockify tracker site")
         }
-
     });
+
+    restoreOptions();
+    document.getElementById('webhook_url').addEventListener('input', saveOptions);
+    document.getElementById('send-button').addEventListener('click', sendToWebhook);
+
 });
 $(function () {
     $("#copy-button").on("click", copy);
@@ -27,13 +29,14 @@ $(function () {
 
 
 function writeToPopup(info) {
-    let link = uploadImage(info.img);
     $(".loading").hide();
     $(".content-wrap").show();
-    $(".entries").html(formatOutput(info.entries, link))
+    $(".entries").html(formatOutput(info.entries, "Loading..."))
+    let link = uploadImage(info.img);
+    // $(".entries").html(formatOutput(info.entries, link))
 };
 
-function formatOutput(entries, link) {
+function formatOutput(entries) {
     let output = "";
 
     let groups = entries.reduce(function (r, a) {
@@ -53,7 +56,7 @@ function formatOutput(entries, link) {
         output += `</ul>`;
         output += '<br>';
     })
-    output += `Clockify report:  <a href="${link}" target="_blank">${link}</a>`;
+    output += `Clockify report: <span class="screenshot"> Loading... </screenshot>`;
 
     return output;
 }
@@ -75,12 +78,13 @@ function uploadImage(img) {
         success: function (response) {
             if (response.success) {
                 link = response.data.link;
+                $(".screenshot").html(`<a href="${link}" target="_blank">${link}</a>`)
             }
         },
         cache: false,
         contentType: false,
         processData: false,
-        async: false,
+        async: true,
     });
 
     return link;
@@ -91,4 +95,56 @@ function copy() {
     const blob = new Blob([content], { type: 'text/html' });
     const clipboardItem = new window.ClipboardItem({ 'text/html': blob });
     navigator.clipboard.write([clipboardItem]).then(() => window.close());
+}
+
+function saveOptions() {
+    const webhook_url = document.getElementById('webhook_url').value;
+    handleSendButton()
+
+    chrome.storage.sync.set(
+        { webhook_url },
+        () => {
+            // Update status to let user know options were saved.
+            const status = document.getElementById('status');
+            status.textContent = 'Options saved.';
+            setTimeout(() => {
+                status.textContent = '';
+            }, 750);
+        }
+    );
+};
+
+function restoreOptions() {
+    chrome.storage.sync.get(
+        { webhook_url: '' },
+        (items) => {
+            document.getElementById('webhook_url').value = items.webhook_url;
+            handleSendButton()
+        }
+    );
+};
+
+function handleSendButton() {
+    if (document.getElementById('webhook_url').value)
+        document.getElementById('send-button').removeAttribute("disabled")
+    else
+        document.getElementById('send-button').setAttribute("disabled", "")
+}
+
+function sendToWebhook() {
+    let url = document.getElementById('webhook_url').value;
+
+    if (!url) return;
+
+    $.ajax({
+        url,
+        type: "POST",
+        contentType: 'application/json',
+        data: JSON.stringify({
+            text: "*Hello*"
+        }),
+        success: function (response) {
+            window.close()
+        },
+    });
 }
